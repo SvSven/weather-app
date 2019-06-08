14cdd6e716cd35eb4285ad715aeb33ee8c296359
Sven van Steenis <sven.vsteenis@gmail.com>
2019-06-08 19:35:02 +0200

    add new blank WeatherCard component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
     <div id="app">
-
+        <WeatherCard v-if="forecast" v-bind:forecast="forecast" />
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import WeatherCard from './components/WeatherCard.vue';
 
 export default {
     name: 'app',
     components: {
-
+        WeatherCard
     },
     data () {
         return {
-            IP_INFO: null
+            IP_INFO: null,
+            forecast: null
         }
     },
     mounted: function() {
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'WeatherCard',
+  props: {
+      forecast: {
+        type: Object,
+        required: true
+      }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
