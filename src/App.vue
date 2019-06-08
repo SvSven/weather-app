6262896f59f9091b1cde105a18d5c6b4049436a8
Sven van Steenis <sven.vsteenis@gmail.com>
2019-06-08 18:00:19 +0200

    get geo data based on client IP address

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,22 @@
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
     name: 'app',
     components: {
 
+    },
+    data () {
+        return {
+            IP_INFO: null
+        }
+    },
+    mounted: function() {
+        axios.get('https://get.geojs.io/v1/ip/geo.json').then(response => {
+            this.IP_INFO = response.data;
+        });
     }
 }
 </script>
