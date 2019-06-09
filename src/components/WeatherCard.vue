248c4afb2f4fe8ad127abf05ffca1181d897cfe0
Sven van Steenis <sven.vsteenis@gmail.com>
2019-06-09 17:31:17 +0200

    display upcoming weather info on weathercard

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -11,6 +11,19 @@
 
             <h2 class="weather__summary__text">{{ current.main.temp | roundTemperature }}&deg; {{ current.weather[0].main }}</h2>
         </div>
+
+        <div class="weather__upcoming">
+
+            <div v-for="(report, index) in upcoming.slice(0, 5)" v-bind:key="index" class="weather__upcoming__item weather__summary">
+
+                <p class="weather__summary__time">{{ report.dt_txt }}</p>
+
+                <div class="weather__summary__icon">
+                    <img :src="`http://openweathermap.org/img/w/${report.weather[0].icon}.png`" alt="">
+                </div>
+
+                <h2 class="weather__summary__text">{{ report.main.temp | roundTemperature }}&deg; {{ report.weather[0].main }}</h2>
+            </div>
         </div>
     </div>
 </template>
@@ -45,17 +58,28 @@ export default {
 
         &__header {
             &__title {
-                font-size: 28px;
+                font-size: 24px;
             }
         }
 
         &__current {
-            margin-top: 5px;
+            margin: 3px 0 20px 0;
+            padding-bottom: 20px;
+            border-bottom: solid 2px #d3d3d3;
+
+            .weather__summary__text {
+                font-size: 48px;
+            }
         }
 
         &__summary {
             display: flex;
+            flex-wrap: wrap;
             align-items: center;
+
+            &__time {
+                width: 100%;
+            }
 
             &__icon {
                 margin-right: 5px;
@@ -63,6 +87,23 @@ export default {
 
             &__text {
                 font-size: 24px;
+            }
+        }
+
+        &__upcoming {
+            display: flex;
+            justify-content: space-between;
+
+            .weather__summary {
+                justify-content: center;
+
+                &__time {
+                    text-align: center;
+                }
+            }
+
+            &__item:not(:last-child) {
+                margin-right: 15px;
             }
         }
     }
